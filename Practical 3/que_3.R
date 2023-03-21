8a2501fe9a18f15fee0be6a8d7a9a9e90441f67d
Dhananjay Kumar Dhani <112551917+dhananjaykdhani@users.noreply.github.com>
2023-03-21 09:20:11 +0530

    Update que_3.R

--- Practical 3/que_3.R
+++ Practical 3/que_3.R
@@ -1,8 +1,4 @@
-# An instructor gave an exam to his class that had an average of 65 and standard deviation of 
-# 13. He decided to assign grades as follows: the top 6% and the bottom 6% will receive A’s 
-# and F’s, respectively. The next 16% in either direction will be given B’s and D’s, and the 
-# remaining students will receive C’s. Assuming that the grades on the exam are normally 
-# distributed, find the cutoffs for each grade level.
+# An instructor gave an exam to his class that had an average of 65 and standard deviation of 13. He decided to assign grades as follows: the top 6% and the bottom 6% will receive A’s and F’s, respectively. The next 16% in either direction will be given B’s and D’s, and the remaining students will receive C’s. Assuming that the grades on the exam are normally distributed, find the cutoffs for each grade level.
 
 # given
 average = 65
